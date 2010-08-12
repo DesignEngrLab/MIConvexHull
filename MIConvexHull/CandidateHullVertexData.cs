6746b98db1414869f9248e8f34aa27ad32f4d095
Prof. Matt Campbell <micampbell@users.noreply.github.com>
2010-08-12 15:05:47 +0200

    I'm driving myself crazy with this! At any rate, I think I'm getting close to the problem with 3D convex hull. It's the FixNonConvexFaces function. It seems to create multiple identical faces; and identical faces with the opposite normal (which is bad). Also, though I'm getting close to figuring out how to extend this to n-dimensions. However, the code for 2D and 3D will remain the way it is (once th bugs are fixed) since it is optimized for these. And because you can't do a cross-product for higher dimensions (yeah, I know, except 7).

--- MIConvexHull/CandidateHullVertexData.cs
+++ MIConvexHull/CandidateHullVertexData.cs
@@ -8,7 +8,7 @@ namespace MIConvexHullPluginNameSpace
     internal class CandidateHullVertexData
     {
         public IVertexConvHull vertex;
-      //  public int faceIndex;
+        public IFaceConvHull containedFace;
         public SortedList<double, IFaceConvHull> otherFaces;
     }
 
@@ -21,7 +21,7 @@ namespace MIConvexHullPluginNameSpace
     {
         public int Compare(double x, double y)
         {
-            if (x < y) return -1;
+            if (x > y) return -1;
             else return 1;
         }
     }
