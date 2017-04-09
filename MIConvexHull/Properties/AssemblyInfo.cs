b76879b4568e7ad04ea2464e4d90b0c52e50f94f
Matt Campbell <mattica@gmail.com>
2017-04-09 16:17:00 -0700

    cleaning up .NET Core and updating to NuGet

--- MIConvexHull/Properties/AssemblyInfo.cs
+++ MIConvexHull/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -8,7 +10,7 @@
 [assembly: AssemblyConfiguration("PCL, .Net4.0, Silverlight5, Win8, WP8.1, WPSilverlight8")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MIConvexHull")]
-[assembly: AssemblyCopyright("Copyright © 2010")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,5 +25,15 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.16.0000")]
-[assembly: AssemblyFileVersion("1.0.16.0000")]
+[assembly: AssemblyVersion("1.0.17.*")]
+[assembly: AssemblyFileVersion("1.0.17.*")]
+
+#if NETSTANDARD1_6
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("907aa3b8-5fa0-413a-8bd3-e6db0e02431a")]
+#endif
