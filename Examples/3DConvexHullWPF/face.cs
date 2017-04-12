62aa861f0f38c94ae306e8ead9c510805806fb16
Matt Campbell <mattica@gmail.com>
2017-04-11 21:33:27 -0700

    various comment corrections and new project .NET Standard 1.0 in lieu or .NET Core and .NET 4.5+

--- Examples/3DConvexHullWPF/face.cs
+++ Examples/3DConvexHullWPF/face.cs
@@ -3,20 +3,20 @@
  *     Copyright 2010 Matthew Ira Campbell, PhD.
  *
  *     MIConvexHull is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
+ *     it under the terms of the MIT License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
  *  
  *     MIConvexHull is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ *     MIT License for more details.
  *  
- *     You should have received a copy of the GNU General Public License
- *     along with MIConvexHull.  If not, see <http://www.gnu.org/licenses/>.
+ *     You should have received a copy of the MIT License
+ *     along with MIConvexHull.
  *     
  *     Please find further details and contact information on GraphSynth
- *     at http://miconvexhull.codeplex.com
+ *     at https://designengrlab.github.io/MIConvexHull/
  *************************************************************************/
 namespace ExampleWithGraphics
 {
--- MIConvexHull/Properties/AssemblyInfo.cs
+++ MIConvexHull/Properties/AssemblyInfo.cs
@@ -27,13 +27,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.17.0411")]
 [assembly: AssemblyFileVersion("1.0.17.0411")]
-
-#if NETSTANDARD1_6
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("907aa3b8-5fa0-413a-8bd3-e6db0e02431a")]
-#endif
