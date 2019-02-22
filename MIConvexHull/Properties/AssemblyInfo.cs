8cea50e49c404a37176454d59bed5b5dbd89b711
Prof. Matt Campbell <matt.campbell@oregonstate.edu>
2019-02-22 12:00:34 -0800

    fixed signing and version number

--- MIConvexHull/Properties/AssemblyInfo.cs
+++ MIConvexHull/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.17.0411")]
-[assembly: AssemblyFileVersion("1.0.17.0411")]
+[assembly: AssemblyVersion("1.1.18.1017")]
+[assembly: AssemblyFileVersion("1.1.18.1017")]
